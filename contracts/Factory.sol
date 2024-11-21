35a82aa3c43708dcc6463bbdad75d8d61871983e
Justin <justin7359900@gmail.com>
2024-11-21 14:11:58 -0500

    Setup token creation

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.28;
+pragma solidity 0.8.27;
 
 import {Token} from "./Token.sol";
 
 contract Factory {
     uint256 public immutable fee;
     address public owner;
+
+    mapping(address => address[]) public creatorToTokens;
 
     event TokenCreated(address indexed token);
 
@@ -14,12 +16,29 @@ contract Factory {
         owner = msg.sender;
     }
 
-    function create(string memory _name, string memory _symbol) external {
-        Token token = new Token{salt: msg.sender}(_name, _symbol);
+    function create(
+        string memory _name,
+        string memory _symbol
+    ) external payable {
+        require(msg.value >= fee, "Factory: Creator fee not met");
+
+        bytes32 salt = keccak256(abi.encodePacked(msg.sender, _name, _symbol));
+
+        Token token = new Token{salt: salt}(
+            msg.sender,
+            _name,
+            _symbol,
+            1_000_000 ether
+        );
+
+        creatorToTokens[msg.sender].push(address(token));
+
         emit TokenCreated(address(token));
     }
 
     function buy(address _token, uint256 _amount) external payable {}
 
     function sell(address _token, uint256 _amount) external {}
+
+    function withdraw() external {}
 }
--- test/Factory.js
+++ test/Factory.js
@@ -1,8 +1,14 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 describe("Factory", function () {
-  const FEE = 10
+  // Factory contract variables
+  const FEE = ethers.parseUnits("0.01", 18)
+
+  // Token contract variables
+  const NAME = "DAPP Uni"
+  const SYMBOL = "DAPP"
 
   async function deployFactoryFixture() {
     // Get accounts
@@ -12,8 +18,16 @@ describe("Factory", function () {
     const Factory = await ethers.getContractFactory("Factory")
     const factory = await Factory.deploy(FEE)
 
+    // Create token
+    const transaction = await factory.connect(creator).create(NAME, SYMBOL, { value: FEE })
+    await transaction.wait()
+
+    // Get token address
+    const tokenAddress = await factory.creatorToTokens(creator.address, 0)
+    const token = await ethers.getContractAt("Token", tokenAddress)
+
     // Return values
-    return { factory, deployer, creator, buyer }
+    return { factory, token, deployer, creator, buyer }
   }
 
   describe("Deployment", function () {
@@ -25,6 +39,39 @@ describe("Factory", function () {
     it("Should set the owner", async function () {
       const { factory, deployer } = await loadFixture(deployFactoryFixture)
       expect(await factory.owner()).to.equal(deployer.address)
+    })
+  })
+
+  describe("Creating", function () {
+    it("Should deploy token contract", async function () {
+      const { token } = await loadFixture(deployFactoryFixture)
+      expect(token.getAddress()).to.not.equal("0x0000000000000000000000000000000000000000")
+    })
+
+    it("Should set the owner", async function () {
+      const { factory, token } = await loadFixture(deployFactoryFixture)
+      expect(await token.owner()).to.equal(await factory.getAddress())
+    })
+
+    it("Should set the creator", async function () {
+      const { token, creator } = await loadFixture(deployFactoryFixture)
+      expect(await token.creator()).to.equal(creator.address)
+    })
+
+    it("Should set the supply", async function () {
+      const { factory, token } = await loadFixture(deployFactoryFixture)
+
+      const totalSupply = ethers.parseUnits("1000000", 18)
+
+      expect(await token.balanceOf(await factory.getAddress())).to.equal(totalSupply)
+    })
+
+    it("Should update ETH balance", async function () {
+      const { factory } = await loadFixture(deployFactoryFixture)
+
+      const balance = await ethers.provider.getBalance(await factory.getAddress())
+
+      expect(balance).to.equal(FEE)
     })
   })
 
